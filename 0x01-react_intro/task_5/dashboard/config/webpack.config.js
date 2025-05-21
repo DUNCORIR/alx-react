c95f877d9771774d35f2933090f95123d61f9be0
duncorir <duncorir@mail.com>
2025-05-21 15:51:27 +0300

    tasks 4 5 react_props

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -1,61 +1,39 @@
+// config/webpack.config.js
+
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, '../src/index.js'),
+  entry: './src/index.js', // ✅ points to actual entry file
   output: {
     path: path.resolve(__dirname, '../dist'),
     filename: 'bundle.js',
+    publicPath: '/',
     clean: true,
   },
-  mode: 'development',
-  devtool: 'inline-source-map',
-  devServer: {
-    static: path.resolve(__dirname, '../dist'),
-    hot: true,
-    port: 3000,
-    open: true,
+  resolve: {
+    extensions: ['.js', '.jsx'],
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ['babel-loader'],
+        use: 'babel-loader',
       },
       {
-        test: /\.css$/,
+        test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|jpe?g|gif|svg)$/i,
         type: 'asset/resource',
-        use: [
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              mozjpeg: {
-                progressive: true,
-              },
-              optipng: {
-                enabled: true,
-              },
-              gifsicle: {
-                interlaced: false,
-              },
-            },
-          },
-        ],
       },
     ],
   },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, '../dist/index.html'),
-      favicon: path.resolve(__dirname, '../dist/favicon.ico'),
+      template: './public/index.html', // ✅ Make sure this file exists
     }),
   ],
 };
