9575431c1ade866750e4d408b3172a03e905672a
duncorir <duncorir@mail.com>
2025-06-05 19:49:30 +0300

    Nested merge

--- 0x06-React_Immutable/6-deeply.js
+++ 0x06-React_Immutable/6-deeply.js
@@ -1,7 +1,33 @@
-import { fromJS } from 'immutable';
-export function mergeDeeplyElements(page1, page2) {
+import { Map, fromJS } from 'immutable';
+import util from 'util';
+
+export default function mergeDeeplyElements(page1, page2) {
     const immutablePage1 = fromJS(page1);
     const immutablePage2 = fromJS(page2);
-    const merged = immutablePage1.mergeDeep(immutablePage2);
-    return merged;
+    
+    return immutablePage1.mergeDeep(immutablePage2);
 }
+
+const page1 = {
+  'user-1': {
+    id: 1,
+    name: 'test',
+    likes: {
+      1: {
+        uid: 1234,
+      }
+    }
+  },
+};
+
+const page2 = {
+  'user-1': {
+    likes: {
+      2: {
+        uid: 134,
+      }
+    }
+  },
+};
+
+console.log(util.inspect(mergeDeeplyElements(page1, page2).toJS(), { depth: null, colors: true }));
