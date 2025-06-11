11a69e87f5098c517ad10b2eaf9329f2e2c8db7e
duncorir <duncorir@mail.com>
2025-06-11 16:32:41 +0300

    Normalize a nested JSON

--- 0x07-react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
+++ 0x07-react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
@@ -17,24 +17,4 @@ const notification = new schema.Entity('notifications', {
 
 // Normalize the data
 export const normalizedData = normalize(notificationsData, [notification]);
-
-// Task 2 prep: Get all notifications by user
-export function getAllNotificationsByUser(userId) {
-  const notifications = normalizedData.entities.notifications;
-  const messages = normalizedData.entities.messages;
-
-  if (typeof userId !== 'string') return [];
-
-  const result = [];
-  for (const id in notifications) {
-    if (notifications[id].author === userId) {
-      const contextId = notifications[id].context;
-      if (messages[contextId]) {
-        result.push(messages[contextId]);
-      }
-    }
-  }
-  return result;
-}
-
 export { user, message, notification };
